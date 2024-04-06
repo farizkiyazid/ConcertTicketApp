2e9110693d3075fee2d3a378e6046ccb036acfa4
Farizki Yazid <yazidfarizki@gmail.com>
2024-04-07 01:30:58 +0700

    init spring boot project with mysql WIP

---
+++ src/main/java/com/example/concertticketapp/ConcertTicketAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.concertticketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertTicketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertTicketAppApplication.class, args);
+	}
+
+}
