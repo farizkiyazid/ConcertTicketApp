652d2bffe71923aaea1d7e5c83eb2b662558fdd6
Farizki Yazid <yazidfarizki@gmail.com>
2024-04-07 11:15:30 +0700

    create model classes

---
+++ src/main/java/com/example/concertticketapp/model/Ticket.java
@@ -0,0 +1,28 @@
+package com.example.concertticketapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long concertId;
+    private Long userId;
+    private LocalDateTime purchaseTime;
+    private int quantity;
+}
+
---
+++ src/main/java/com/example/concertticketapp/model/User.java
@@ -0,0 +1,23 @@
+package com.example.concertticketapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+}
